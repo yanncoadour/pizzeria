8c2f1f1570e14419b374eeefd981fbf8895f4a20
yanncoadour <yann.coadour@gmail.com>
2023-04-08 18:31:15 +0200

    page acceuil 60%

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AcceuilComponent} from "./acceuil/acceuil.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AcceuilComponent }, // Page d'accueil
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    AcceuilComponent,
 
   ],
   imports: [
